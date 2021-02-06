f1ff706f7e942ba625ce97654681a32c0d10e816
Faruk Onar <farukoonar@gmail.com>
2021-02-07 00:00:20 +0300

    6 Şubat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,6 +11,24 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //IoC
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             //foreach (var product in productManager.GetAll())
@@ -20,14 +38,17 @@ namespace ConsoleUI
 
             //foreach (var product in productManager.GetAllByCategory(2))
             //{
-            //    Console.WriteLine(product.ProductName+" "+product.CategoryId);
+            //    Console.WriteLine(product.ProductName + " " + product.CategoryId);
             //}
 
-            foreach (var product in productManager.GetAllByUnitPrice(50,100))
+            //foreach (var product in productManager.GetAllByUnitPrice(50, 100))
+            //{
+            //    Console.WriteLine(product.ProductName + " " + product.UnitPrice);
+            //}
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName + " " + product.UnitPrice);
+                Console.WriteLine(product.ProductName+" / " + product.CategoryName);
             }
-
         }
     }
 }
